c05579e2d1c365ec90a1fdc1c4a73251ef5e2f8e
Alois MAIER <alois@maier.co>
2022-02-16 14:01:35 +0100

    ajout des images et implémentation dans le code

--- menu.py
+++ menu.py
@@ -1,1 +1,8 @@
 import pygame
+import functions
+
+class Menu:
+    def __init__(self):
+        self.last_click = "nothing"
+        self.image_menu = pygame.image.load("menu.png").convert()
+        self.button_play = pygame.image.load("play_idle.png").convert_alpha()
