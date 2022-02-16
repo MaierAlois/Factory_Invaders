881ab5676aacaebebfd08c066af9897a02beff79
Alois MAIER <alois@maier.co>
2022-02-16 14:11:23 +0100

    ajout des fonctions principales du menu

--- menu.py
+++ menu.py
@@ -2,7 +2,26 @@ import pygame
 import functions
 
 class Menu:
-    def __init__(self):
+    def __init__(self, screen):
+        self.running = True
+        self.screen = screen
         self.last_click = "nothing"
         self.image_menu = pygame.image.load("menu.png").convert()
         self.button_play = pygame.image.load("play_idle.png").convert_alpha()
+    
+    def handling_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+    
+    def update(self):
+        """oui"""
+        
+    def display(self):
+        self.screen.blit(self.image_menu, (0, 0))
+        self.screen.blit(self.button_play, (0, 0))
+    
+    def loop(self):
+        while self.running:
+            self.update()
+            self.display()
